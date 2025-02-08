433f6b436055aaa85ce72b69610932b57a332334
Nithinkumar-05 <nithineruventi@outlook.com>
2025-02-08 09:46:36 +0530

    `feat: updated user tabs and more screen with new features and functionality`

--- Project/app/(protected)/(user)/(tabs)/more.js
+++ Project/app/(protected)/(user)/(tabs)/more.js
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native';
-
+import { View, Text, TouchableOpacity } from 'react-native';
+import {useAuth} from '@/routes/AuthContext';
 export default function More() {
+  const {logout} = useAuth();
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl font-bold">More Screen</Text>
+      <TouchableOpacity className="bg-black shadow-lg rounded-xl p-3" onPress={logout}>
+        <Text className="text-white text-center">Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 }
